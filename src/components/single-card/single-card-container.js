a1cb2cd013c96bec375d58aef4ba02ff430ca3fd
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2024-03-28 23:31:31 +0200

    fix

---
+++ src/components/single-card/single-card-container.js
@@ -0,0 +1,56 @@
+import { getBackdrop, getPoster, getYear } from '../../utils';
+import { ActionsContainer } from '../actions';
+import { Genres } from '../genres';
+import { AverageRatingInfo } from '../ratings';
+
+export default function SingleCardContainer({
+  backdrop_path,
+  mediaType,
+  id,
+  poster_path,
+  title,
+  year,
+  vote_average,
+  vote_count,
+  genres,
+  overview,
+}) {
+  return (
+    <div className='single-card-container'>
+      <img
+        className='blur_back single-card-image'
+        src={getBackdrop(backdrop_path)}
+        alt='Single card'
+      />
+      <div className='single-card-info-wrapper'>
+        <div className='single-card-header'>
+          <a href={`${mediaType}/${id}`}>
+            <picture>
+              <img
+                className='single-card-poster'
+                src={getPoster(poster_path)}
+                alt='Single card poster'
+              />
+            </picture>
+          </a>
+          <div className='single-card-info-column'>
+            <a href={`${mediaType}/${id}`}>
+              <h2 className='single-card-title'>
+                {title} ({getYear(year)})
+              </h2>
+            </a>
+            <AverageRatingInfo
+              className='average-card-ratings'
+              vote_average={vote_average}
+              vote_count={vote_count}
+            />
+            <Genres genres={genres} />
+          </div>
+        </div>
+
+        <div className='single-card-description'>{overview}</div>
+        <ActionsContainer addToList={false} wlMinimal />
+      </div>
+    </div>
+  );
+}
--- src/components/single-card/single-card.js
+++ src/components/single-card/single-card.js
@@ -1,68 +1,25 @@
 import './single-card.css';
-import { ActionsContainer } from '../actions';
+
 import { useSingleContentLoader } from '../../hooks';
-import { getBackdrop, getPoster, getYear } from '../../utils';
-import { Genres } from '../genres';
 import { memo } from 'react';
-import { AverageRatingInfo } from '../ratings';
+import SingleCardContainer from './single-card-container';
 
 const SingleCard = memo(({ id, mediaType }) => {
   const { data, loading, error } = useSingleContentLoader(id, mediaType);
 
   if (error) return null;
 
-  const {
-    genres,
-    backdrop_path,
-    poster_path,
-    overview,
-    release_date,
-    last_air_date,
-    vote_average,
-    vote_count,
-  } = data || {};
+  const { release_date, last_air_date } = data || {};
 
   return (
     <div className='single-card-wrapper'>
       <div className='single-card'>
         {!loading && (
-          <div className='single-card-container'>
-            <img
-              className='blur_back single-card-image'
-              src={getBackdrop(backdrop_path)}
-              alt='Single card'
-            />
-            <div className='single-card-info-wrapper'>
-              <div className='single-card-header'>
-                <a href={`${mediaType}/${id}`}>
-                  <picture>
-                    <img
-                      className='single-card-poster'
-                      src={getPoster(poster_path)}
-                      alt='Single card poster'
-                    />
-                  </picture>
-                </a>
-                <div className='single-card-info-column'>
-                  <a href={`${mediaType}/${id}`}>
-                    <h2 className='single-card-title'>
-                      {data?.title || data?.name} (
-                      {getYear(release_date || last_air_date)})
-                    </h2>
-                  </a>
-                  <AverageRatingInfo
-                    className='average-card-ratings'
-                    vote_average={vote_average}
-                    vote_count={vote_count}
-                  />
-                  <Genres genres={genres} />
-                </div>
-              </div>
-
-              <div className='single-card-description'>{overview}</div>
-              <ActionsContainer addToList={false} wlMinimal />
-            </div>
-          </div>
+          <SingleCardContainer
+            {...data}
+            title={data?.title || data?.name}
+            year={release_date || last_air_date}
+          />
         )}
       </div>
     </div>
