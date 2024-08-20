0869a14ae34c569e816f0da072b3bcea855ff965
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2024-08-21 00:23:25 +0300

    Bug fixed

--- src/components/header/search-input.js
+++ src/components/header/search-input.js
@@ -1,16 +1,33 @@
-import { useEffect, useRef } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 
 export default function SearchInput() {
   const navigate = useNavigate();
   const location = useLocation();
-  const ref = useRef();
+  const [value, setValue] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const handleInputChange = event => {
-    const value = event.target.value;
-    const url = value === '' ? '/search' : `/search?q=${value}`;
-    navigate(url);
-  };
+  const removeParam = useCallback(
+    key => {
+      searchParams.delete(key);
+      setSearchParams(searchParams);
+    },
+    [searchParams, setSearchParams],
+  );
+
+  const handleInputChange = useCallback(
+    event => {
+      const value = event.target.value;
+
+      setValue(value);
+      if (value === '') {
+        removeParam('q');
+      } else {
+        setSearchParams({ q: value });
+      }
+    },
+    [removeParam, setSearchParams],
+  );
 
   const handleClick = () => {
     if (location.pathname !== '/search') {
@@ -19,23 +36,24 @@ export default function SearchInput() {
   };
 
   useEffect(() => {
-    const inputRef = ref.current;
-    const params = new URLSearchParams(location.search);
-    inputRef.value = params.get('q') || '';
+    setValue(searchParams.get('q') || '');
+    if (searchParams.get('q') === '') {
+      removeParam('q');
+    }
     return () => {
-      inputRef.value = '';
+      setValue('');
     };
-  }, [location]);
+  }, [location, removeParam, searchParams, setSearchParams]);
 
   return (
     <div className='search'>
       <div id='cover'>
         <input
+          value={value}
           type='text'
           placeholder='Search...'
           onClick={handleClick}
           onChange={handleInputChange}
-          ref={ref}
         />
       </div>
     </div>
