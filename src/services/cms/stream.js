34f5ca18fc90210212b44581874f8388deb9d595
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2024-09-22 23:46:22 +0300

    Changed endpoint

--- src/services/cms/stream.js
+++ src/services/cms/stream.js
@@ -1,7 +1,7 @@
 import config from '../../config';
 
 export async function getStream({ content, translation }) {
-  const url = config.api + '/cms/stream';
+  const url = config.api + '/cms/sources';
   const response = await fetch(url, {
     method: 'POST',
     headers: {
