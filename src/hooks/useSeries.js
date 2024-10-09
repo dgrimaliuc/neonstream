1b3d13200b45ae8bc4b2bad52657edbe52aa2fc1
Denis Grimaliuc <94832020+dgrimaliuc-crunchyroll@users.noreply.github.com>
2024-10-09 17:37:50 +0300

    VOD player fixes (#23)

--- src/hooks/useSeries.js
+++ src/hooks/useSeries.js
@@ -1,24 +1,18 @@
 import { seriesActions } from '../store';
 import { useSelector } from 'react-redux';
-import { seriesData, seriesTranslations } from '../store/selectors/series';
+import { seriesData } from '../store/selectors/series';
 import { useDispatchAction } from './useDispatchAction';
 import { useEffect } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import { useLoaderData } from 'react-router-dom';
 import { loadTv } from '../pages';
-import { isObjEmpty } from '../utils';
-function fetchTranslations() {
-  // TODO remove
-}
 
 export default function useSeries() {
   const dispatch = useDispatchAction(seriesActions);
   const persistData = useSelector(seriesData);
-  const translations = useSelector(seriesTranslations);
   const loaderData = useLoaderData();
   const params = useParams();
   const { pathname } = useLocation();
-  const season_number = +params.season || 1;
 
   // Set Series selector data
   useEffect(() => {
@@ -31,40 +25,5 @@ export default function useSeries() {
     })();
   }, [persistData, dispatch, loaderData, params, pathname]);
 
-  //Set Translations selector data
-  useEffect(() => {
-    if (
-      +params.id === persistData.id &&
-      (translations.id !== +params.id ||
-        (season_number !== translations.season_number &&
-          !isObjEmpty(translations.rezka2.extract.seasons)))
-    ) {
-      // dispatch.removeTranslations()();
-      fetchTranslations({
-        // May return invalid season as season 0 can be "Specials" not the first season
-        content: { ...persistData, season_number },
-        //setLoadingState: dispatch.setLoading,
-        setAudioSources: translations =>
-          // May return invalid season as season 0 can be "Specials" not the first season
-          dispatch.setTranslations({ translations, season_number })(),
-      });
-    } else if (
-      translations.id &&
-      !isObjEmpty(translations.rezka2?.extract?.seasons) &&
-      (translations.id !== +params.id || season_number !== translations.season_number)
-    ) {
-      dispatch.removeTranslations()();
-    }
-  }, [
-    dispatch,
-    params.id,
-    params.season,
-    persistData,
-    season_number,
-    translations.id,
-    translations.rezka2,
-    translations.season_number,
-  ]);
-
-  return { series: persistData, translations: translations.rezka2 };
+  return { series: persistData };
 }
--- src/services/cms/translation.js
+++ src/services/cms/translation.js
@@ -7,18 +7,7 @@ export async function getTranslations(data) {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      id: data.id,
-      title: data.title,
-      original_title: data.original_title,
-      imdb_id: data.imdb_id,
-      origin_country: data.origin_country,
-      release_date: data.release_date,
-      translations: data.translations,
-      media_type: data.media_type,
-      media_sub_type: data.media_sub_type,
-      downloadIfMissing: true,
-    }),
+    body: JSON.stringify(data),
   });
 
   return await response.json();
