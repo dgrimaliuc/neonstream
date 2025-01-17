a0335752145b9fec5c52b5d4fdef60817e6859d1
Denis Grimaliuc <94832020+dgrimaliuc-crunchyroll@users.noreply.github.com>
2025-01-17 18:21:49 +0200

    Hotfix header height (#35)

--- src/components/header/components/header.js
+++ src/components/header/components/header.js
@@ -5,16 +5,19 @@ import SearchBar from './search';
 
 export default function Header() {
   return (
-    <header className={styles['header_container']}>
-      <div className={styles['header_container__column']}>
-        <div className={styles['header_container__primary']}>
-          <Logo />
-          <SearchBar />
-          <Login />
-        </div>
+    <>
+      <header className={styles['header_container']}>
+        <div className={styles['header_container__column']}>
+          <div className={styles['header_container__primary']}>
+            <Logo />
+            <SearchBar />
+            <Login />
+          </div>
 
-        <Navigation />
-      </div>
-    </header>
+          <Navigation />
+        </div>
+      </header>
+      <div className={styles.spacer} />
+    </>
   );
 }
