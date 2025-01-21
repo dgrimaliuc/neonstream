bc250b6be9aeba769fa7bd4b9cbcf543d45297f0
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2025-01-22 01:33:02 +0200

    f

--- src/components/meta/meta.js
+++ src/components/meta/meta.js
@@ -7,18 +7,12 @@ export default function Meta({ title, overview, poster_path, still_path }) {
       <title>{title}</title>
       <meta property='og:title' content={title} />
       <meta property='og:description' content={overview} />
-      <meta name='title' content={title} />
-      <meta name='description' content={overview} />
-      <meta
-        property='og:image'
-        content={
-          poster_path
+      <meta property='og:image' content={`${window.location.origin}/logo.svg`} />
+      {/* poster_path
             ? getPoster(poster_path, 3)
             : still_path
             ? getStill(still_path, 2)
-            : `${window.location.origin}/logo.svg`
-        }
-      />
+            : */}
       <meta property='og:url' content={`${window.location.origin}${window.location.pathname}`} />
       <meta property='og:type' content='website' />
     </Helmet>
