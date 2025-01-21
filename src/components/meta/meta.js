2fc9218c283f1e01a86ea95e4a619559908fc40f
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2025-01-22 01:46:40 +0200

    f

--- src/components/meta/meta.js
+++ src/components/meta/meta.js
@@ -7,7 +7,12 @@ export default function Meta({ title, overview, poster_path, still_path }) {
       <title>{title}</title>
       <meta property='og:title' content={title} />
       <meta property='og:description' content={overview} />
-      <meta property='og:image' content={`${window.location.origin}/logo.svg`} />
+      <meta
+        property='og:image'
+        content='https://neon-stream--pr36-hero-carousel-refact-oiwetk6s.web.app/logo.svg'
+      />
+      {/* <meta property='og:image' content={`${window.location.origin}/logo.svg`} /> */}
+      <meta property='og:image:width' content='86' data-rh='true'></meta>
       {/* poster_path
             ? getPoster(poster_path, 1)
             : still_path
