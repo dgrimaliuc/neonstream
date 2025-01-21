8e84f72e51ef378b52cd908f626e876c6c752866
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2025-01-22 01:10:53 +0200

    Fixes

--- src/components/meta/meta.js
+++ src/components/meta/meta.js
@@ -7,17 +7,15 @@ export default function Meta({ title, overview, poster_path, still_path }) {
       <title>{title}</title>
       <meta property='og:title' content={title} />
       <meta property='og:description' content={overview} />
+      <meta name='title' content={title} />
+      <meta name='description' content={overview} />
       <meta
         property='og:image'
         content={poster_path ? getPoster(poster_path, 3) : getStill(still_path, 2)}
       />
       {/* <meta property='og:image:width' content='300' />
       <meta property='og:image:height' content='300' /> */}
-      {/* <meta
-      name="description"
-      content="Web site created using create-react-app"
-    /> */}
-      <meta property='og:url' content={window.location.href} />
+      <meta property='og:url' content={`${window.location.origin}${window.location.pathname}`} />
       <meta property='og:type' content='website' />
     </Helmet>
   );
