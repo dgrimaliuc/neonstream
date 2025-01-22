bf66382bf1ffb7ddae844988c39935b1b48781bf
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2025-01-22 10:51:41 +0200

    Ff

--- src/components/meta/meta.js
+++ src/components/meta/meta.js
@@ -1,5 +1,5 @@
 import { Helmet } from 'react-helmet';
-// import { getPoster, getStill } from 'utils';
+import { getPoster, getStill } from 'utils';
 
 export default function Meta({ title, overview, poster_path, still_path }) {
   return (
@@ -9,16 +9,15 @@ export default function Meta({ title, overview, poster_path, still_path }) {
       <meta property='og:description' content={overview} />
       <meta
         property='og:image'
-        content='https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=70,width=1200,height=675/catalog/crunchyroll/93216d743b442175f0a06e5693ae4439.jpg'
-      />
-      {/* <meta property='og:image' content={`${window.location.origin}/logo.svg`} /> */}
-      <meta property='og:image:width' content='1200' data-rh='true'></meta>
-      {/* poster_path
+        content={
+          poster_path
             ? getPoster(poster_path, 1)
             : still_path
             ? getStill(still_path, 1)
-            : */}
-
+            : '/./logo.svg'
+        }
+      />
+      <meta property='og:image:width' content='1200' data-rh='true'></meta>
       <meta property='og:url' content={`${window.location.origin}${window.location.pathname}`} />
       <meta property='og:type' content='website' />
     </Helmet>
