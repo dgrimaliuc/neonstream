e7352b10432f3565375c1751c0cb3107c0aa0f3e
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2025-01-23 22:54:07 +0200

    Fixed player on request failure

--- src/components/player/vod-player.js
+++ src/components/player/vod-player.js
@@ -1,26 +1,23 @@
 import styles from './player.module.css';
 import Player from './player';
 import { Translations } from '../translations';
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import { useHistory, usePlayerControls } from '../../hooks';
 import useTranslations from '../../hooks/useTranslations';
 import VODPlayerPlaceholder from './placeholder-vod-player';
 import useStream from '../../hooks/useStream';
 import usePlayerState from '../../hooks/usePlayerState';
-import { getYoutubeUrl, selectMainTrailer } from '../../api';
-import { TV } from '../../data/constants';
 import VODPlayerUnavailable from './vod-player-unavailable';
 
 export default function VODPlayer({ content, width, height }) {
   const ref = useRef(null);
-  const [trailer, setTrailer] = useState(null);
 
   const { savePlayhead, removePlayhead, getPlayhead, saveCurrentTime } = useHistory({
     content,
     ref,
   });
 
-  const { isTranslationsLoading, translationsData, translationsError, selected, setSelected } =
+  const { isTranslationsLoading, translationsData, selected, setSelected } =
     useTranslations(content);
 
   const { isPlaying, setIsPlaying, handlePlay, handlePause, isReady, setIsReady } =
@@ -37,51 +34,35 @@ export default function VODPlayer({ content, width, height }) {
     savePlayhead,
   });
 
-  const { streamIsLoading, streamData, streamError } = useStream(
+  const { streamIsLoading, streamData } = useStream(
     content,
     translationsData?.translations ? translationsData?.translations[selected] : null,
   );
 
-  useEffect(() => {
-    if (!streamData && content.videos) {
-      const trailer = selectMainTrailer(content.videos);
-      if (trailer?.key) {
-        setTrailer(getYoutubeUrl(trailer.key));
-      }
-    }
-  }, [streamData, content.videos]);
-
-  if (isTranslationsLoading || !translationsData) {
+  if (isTranslationsLoading) {
     return <VODPlayerPlaceholder />;
   }
 
-  const isStreamUnavailable = !streamIsLoading && (!streamData || streamError);
+  const isStreamUnavailable = !streamIsLoading && !streamData;
 
   return (
     <>
       <div className={styles['player-wrapper']}>
         <div className={styles['player-header']}>
           <Translations
-            translations={translationsData.translations}
+            translations={translationsData?.translations}
             selected={selected}
             onClick={setSelected}
           />
         </div>
         <div className={styles['player-container']}>
-          {translationsError && <div className={styles['player-error']}>{translationsError}</div>}
           {isStreamUnavailable && <VODPlayerUnavailable img={content.backdrop_path} />}
           {streamData && (
             <Player
               ref={ref}
               autoPlay={true}
               controls
-              url={
-                !streamIsLoading && streamData && !streamError
-                  ? Object.values(streamData?.qualitys)[0]
-                  : content.media_type !== TV
-                  ? trailer
-                  : null
-              }
+              url={Object.values(streamData?.qualitys)[0]}
               playing={isPlaying}
               onPause={handlePause}
               progressInterval={20000}
